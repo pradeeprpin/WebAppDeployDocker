e0a76022955fc53cd2a8b326c960648a5462734c
Anto shreya <151666094+antoshreya@users.noreply.github.com>
2025-08-30 15:46:08 +0530

    Create install.sh

---
+++ deploy-scripts/install.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+set -e
+
+# Go to app folder
+cd /home/ubuntu/app
+
+# Stop existing container if running
+docker stop web-container || true
+docker rm web-container || true
+
+# Build new image
+docker build -t sample-website .
+
+# Run container
+docker run -d -p 80:80 --name web-container sample-website
